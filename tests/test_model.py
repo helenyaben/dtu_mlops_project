1b93fa4e64469ac867f827f1383315d344d9c24b
davidimmanuelhartel <david.immanuel.hartel@gmail.com>
2023-01-13 16:02:15 +0100

    added tests and coverage report

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,43 +1,40 @@
-import pytest
-import torch
-
 from src.models.model import MyAwesomeModel
-
+import torch
+import pytest
+from torch import nn
+import numpy as np
 
 class TestModel:
     # mark.parametrize enables parametrization of arguments for a test function
     # checks if certain input has the correct shape
-    @pytest.mark.parametrize(
-        "test_input,expected",
-        [
-            (torch.randn(1, 2, 28), ValueError),
-            (torch.randn( 1, 28, 2), ValueError),
-            (torch.randn(100, 3, 28, 28), ValueError),
-        ],
-    )
-    def test_model_input_shape(self, test_input, expected):
-        model = MyAwesomeModel()
-        state_dict = torch.load("src/models/my_trained_model.pt")
-        model.load_state_dict(state_dict)
-
-        # if the input has the wrong shape, the model should raise a ValueError
-        with pytest.raises(
-            expected, match="Expected each x sample to have shape 1,28,28"
-        ):
-            model.forward(test_input)
+    # @pytest.mark.parametrize(
+    #     "test_input,expected",
+    #     [
+    #         (torch.rand((128, 128)), RuntimeError),
+    #     ],
+    # )
+    # def test_model_input_shape(self, test_input, expected):
+    #     model = MyAwesomeModel()
+    #     # if the input has the wrong shape, the model should raise a ValueError
+    #     with pytest.raises(
+    #         expected, match="Expected each x sample to have shape 1,128,128"
+    #     ):
+    #         model.forward(test_input)
 
     # checks if the output shape is correct
     @pytest.mark.parametrize(
         "test_input,expected",
         [
-            (torch.randn(128, 128), (1)),
-            (torch.randn(64, 128, 128), (64, 1)),
-            (torch.randn(100, 1, 128, 128), (100, 1)),
+            (torch.rand((64, 1, 128, 128)), torch.Size([64, 12])),
+            (torch.rand((1,1, 128, 128)), torch.Size([1,12])),
+            (torch.rand((100,1, 128, 128)), torch.Size([100,12])),
         ],
     )
     def test_model_output(self, test_input, expected):
         model = MyAwesomeModel()
-        state_dict = torch.load("src/models/my_trained_model.pt")
-        model.load_state_dict(state_dict)
-        
-        assert output.shape == expected
+        output = model(test_input)
+        assert output.shape == expected, f"Expected output shape {expected}, got {output.shape}"
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
